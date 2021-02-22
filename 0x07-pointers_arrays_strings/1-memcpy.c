7aded45fb0bb1eee2d0586706175628539c716e1
Adriana Echeverri <2903@holbertonschool.com>
2021-02-22 20:00:36 +0000

    betty correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int counter = 0;
 
-	for(; counter < n; counter++)
+	for (; counter < n; counter++)
 	{
 		dest[counter] = src[counter];
 	}
