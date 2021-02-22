4970e27b6f79441f457ecefa8f1de71fdde75e94
Adriana Echeverri <2903@holbertonschool.com>
2021-02-22 20:42:49 +0000

    Null case corrected

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - searches a string for any of a set of bytes..
@@ -33,5 +34,12 @@ char *_strpbrk(char *s, char *accept)
 			break;
 		}
 	}
-	return (s + counter);
+	if (counter > 0)
+	{
+		return (s + counter);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
