0622c71ee12cd70e71b0012597ef0d4d5e079462
Adriana Echeverri <2903@holbertonschool.com>
2021-02-22 22:59:17 +0000

    C - pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,26 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
+ * print_diagsums - prints sum of the diagonals.
+ * @a: matrix in memory as arrays. 
+ * @size: size of each array.
  *
- *
- *
- *
- *
+ * Return: None.
  */
-PROTOTYPE
+void print_diagsums(int *a, int size)
 {
+	int n, sum = 0;
+	int sum2 = 0;
+	int d1 = 0;
+	int d2 = (size - 1);
 
-
+	for (n = 0; n < size; n++)
+	{
+ 		sum += a[d1];
+		sum2 += a[d2];
+		d1 += (1 + size);
+		d2 += (size - 1);
+	}
+	printf("%d, %d\n", sum, sum2);
 }
