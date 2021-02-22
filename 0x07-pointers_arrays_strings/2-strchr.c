f1fba771e4301f5da6ffb5641a6c64445123cd81
Adriana Echeverri <2903@holbertonschool.com>
2021-02-22 20:33:37 +0000

    Null case corrected

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strchr - locates a character in a string.
@@ -23,5 +24,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return (s + len);
+	return (NULL);
 }
