d29209a11afd8c115781821a2cb526682f2cd874
Adriana Echeverri <2903@holbertonschool.com>
2021-02-22 21:56:31 +0000

    C - pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,23 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
+ * print_chessboard - prints chessboard.
+ * @a: row size.
  *
- *
- *
- *
- *
+ * Return: None.
  */
-PROTOTYPE
+void print_chessboard(char (*a)[8])
 {
+	int i, j, rows;
 
-
+	rows = sizeof(a);
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
