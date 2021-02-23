8ec57af3e1d89a5dade3424f18a1bcec176fb33f
Adriana Echeverri <2903@holbertonschool.com>
2021-02-23 01:13:24 +0000

    Case empty string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[0])
 		{
 			coinc = 0;
-			for (j = 0; (haystack[i + j] == needle[j]) && (haystack[i + j] != '\0'); j++)
+			for (j = 0; (haystack[i + j] == needle[j]); j++)
 			{
 				coinc++;
 				if (coinc == len)
@@ -29,6 +29,10 @@ char *_strstr(char *haystack, char *needle)
 					return (haystack + i);
 				}
 			}
+		}
+		else if (len == 0)
+		{
+			return (haystack);
 		}
 	}
 	return (NULL);
