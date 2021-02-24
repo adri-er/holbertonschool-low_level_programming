310de41b72347cc92975601c2c9b76056977e20e
Adriana Echeverri <2903@holbertonschool.com>
2021-02-24 22:48:28 +0000

    New file to calculate power recursively

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,25 @@
 #include "holberton.h"
 
 /**
- * func - .
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: value to be powered.
+ * @y: the power, times x is multiplied.
  *
- *
- * Return:  .
+ * Return: the power y of x or -1 if y is lower than 0.
  */
-prototype
+int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+	{
+		return (1);
+	}
 
 }
