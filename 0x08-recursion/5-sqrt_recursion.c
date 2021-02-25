d06578f93f7b316014814f60dd780aa3dc985a00
Adriana Echeverri <2903@holbertonschool.com>
2021-02-25 00:08:16 +0000

    New file to estimate sqaure root with recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,39 @@
 #include "holberton.h"
 
 /**
- * func - .
+ * _sqrt - returns the natural square root of a number.
+ * @num: number to calculate square root.
+ * @count: number to evaluate if its the root.
  *
- *
- * Return:  .
+ * Return: square root of a number or -1 if it doesn´t have.
  */
-prototype
+int _sqrt(int num, int count)
 {
+	if (count * count != num)
+	{
+		if (count > 0)
+		{
+			return (_sqrt(num, count - 1));
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	else
+	{
+		return (count);
+	}
+}
 
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to calculate square root.
+ *
+ * Return: square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, n));
 }
