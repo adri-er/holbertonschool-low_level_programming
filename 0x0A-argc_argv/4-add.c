44da443a38a9bb3cd3a6eafb300352032b8fea39
Adriana Echeverri <2903@holbertonschool.com>
2021-02-28 01:23:32 +0000

    External conditional removal

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,22 +13,17 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc <= 1)
+        for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (atoi(argv[i]) == 0 && *argv[i] != '0')
 		{
-			if (atoi(argv[i]) == 0 && *argv[i] != '0')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }
