3f24cdca41b20d4300ab6067d4eb60b63b5afe73
Adriana Echeverri <2903@holbertonschool.com>
2021-03-02 22:56:07 +0000

    Malloc proyect excesices

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,35 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array of integers done.
+ * @height: height of the array of integers.
  *
- *
- *
- *
+ * Return: NULL on failure or if w and h are 0 or pointer to array.
  */
-PROTOTYPE
+int **alloc_grid(int width, int height)
 {
+	int index, index_j;
+	int **memory;
 
+	memory = (int **)malloc(sizeof(int *) * height);
+	if (memory == NULL || width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < height; index++)
+	{
+		memory[index] = (int *)malloc(sizeof(int) * width);
+	}
 
+	for (index = 0; index < height; index++)
+	{
+		for (index_j = 0; index_j < width; index_j++)
+		{
+			memory[index][index_j] = 0;
+		}
+	}
+	return (memory);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- *
- *
+ * free_grid - frees a 2 dimensional grid previously created.
+ * @grid: grid to free malloc.
+ * @height: height of the grid.
  *
  */
-PROTOTYPE
+void free_grid(int **grid, int height)
 {
+	int index;
 
-
+	for (index = 0; index < height; index++)
+	{
+		free(grid[0]);
+	}
 }
