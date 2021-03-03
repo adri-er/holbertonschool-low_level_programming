d2d4d704f6515801e83696b531855cb4c5381b2c
Adriana Echeverri <2903@holbertonschool.com>
2021-03-03 03:55:52 +0000

    Advanced task malloc

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "holberton"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words.
+ * @str: String to be separated.
+ *
+ * Return: A pointer to an array of strings.
+ */
+char **strtow(char *str)
+{
+	return (NULL);
+}
