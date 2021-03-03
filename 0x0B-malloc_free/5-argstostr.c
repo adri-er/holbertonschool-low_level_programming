75551a5379a3e2a286886557f496aca60d072c72
Adriana Echeverri <2903@holbertonschool.com>
2021-03-03 03:23:44 +0000

    argv concatenate in memory

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,13 +1,42 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: number of entries.
+ * @av: pointers to entries.
  *
- *
- *
- *
+ * Return: entries concatenated.
  */
-PROTOTYPE
+char *argstostr(int ac, char **av)
 {
+	char *memory;
+	int counter = 0, amount = 0, length = 0, index;
 
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
+	for (; counter < ac; counter++)
+	{
+		amount += sizeof(av[counter]);
+	}
+	memory = (char *)malloc(amount);
+	ac--;
+	for (; ac >= 0; ac--)
+	{
+		length = sizeof(av[ac]);
+		for (index = 0; index < length; index++)
+		{
+			memory[counter] = *(av[ac] + index);
+			counter++;
+			if (index + 1 == length)
+			{
+				memory[counter] = '\n';
+			}
+		}
+	}
+	return (memory);
 }
