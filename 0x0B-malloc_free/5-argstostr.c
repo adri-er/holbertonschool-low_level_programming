10a5fa1e501dcfe01733681bb000fcbaa2dcfc48
Adriana Echeverri <2903@holbertonschool.com>
2021-03-03 03:50:40 +0000

    Correction in name of an index

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -12,29 +12,41 @@
 char *argstostr(int ac, char **av)
 {
 	char *memory;
-	int counter = 0, amount = 0, length = 0, index;
+	int counter = 0, amount = 0, length = 0, index, index2, index3;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	for (; counter < ac; counter++)
+	for (index2 = 0; index2 < ac; index2++)
 	{
-		amount += sizeof(av[counter]);
+		for (length = 0; av[index2][length]; length++)
+		{
+		}
+		amount += (length + 1);
 	}
-	memory = (char *)malloc(amount);
-	ac--;
-	for (; ac >= 0; ac--)
+
+	memory = malloc((amount + 1) * sizeof(char));
+	if (memory == NULL)
 	{
-		length = sizeof(av[ac]);
+		return (NULL);
+	}
+
+	for (index3 = 0; index3 < ac; index3++)
+	{
+		for (length = 0; av[index3][length]; length++)
+		{
+		}
+
 		for (index = 0; index < length; index++)
 		{
-			memory[counter] = *(av[ac] + index);
+			memory[counter] = av[index3][index];
 			counter++;
 			if (index + 1 == length)
 			{
 				memory[counter] = '\n';
+				counter++;
 			}
 		}
 	}
