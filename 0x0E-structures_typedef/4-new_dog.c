cbe97223d5e4b9e37a852c4f22266472d4b5730a
Adriana Echeverri <2903@holbertonschool.com>
2021-03-09 00:18:11 +0000

    Create a copy in heap

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char *_strdup(char *str);
+
 /**
  * new_dog - function that creates a new dog.
  * @name: Name of new dog.
@@ -12,15 +14,52 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new_dog_;
+	char *name_c = _strdup(name);
+	char *owner_c = _strdup(owner);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	new_dog_ = malloc(sizeof(dog_t));
+	if (new_dog_ == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+	new_dog_->name = name_c;
+	new_dog_->age = age;
+	new_dog_->owner = owner_c;
+	return (new_dog_);
+}
+
+/**
+ * _strdup - returns pointer to copy of a string given.
+ * @str: string thats going to be copied in memory.
+ *
+ * Return: pointer to space in memory with copied string.
+ */
+char *_strdup(char *str)
+{
+	char *memory;
+	int index, length = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[length])
+	{
+		length++;
+	}
+	length++;
+	memory = malloc(length * sizeof(char));
+	if (memory != NULL)
+	{
+		for (index = 0; index <= length; index++)
+		{
+			memory[index] = str[index];
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (memory);
 }
