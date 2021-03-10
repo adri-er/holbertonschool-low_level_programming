81be7c659600117079b7a7696d44414fdab8cfbf
Adriana Echeverri <2903@holbertonschool.com>
2021-03-10 19:02:51 +0000

    Null cases of pointers added

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,11 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (index = 0; index < size; index++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[index]) != 0)
+		for (index = 0; index < size; index++)
 		{
-			return (index);
+			if (cmp(array[index]) != 0)
+			{
+				return (index);
+			}
 		}
 	}
 	return (-1);
