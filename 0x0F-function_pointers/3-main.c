a72f5391a3044824ef8f5a7ff299af1aff166dc0
Adriana Echeverri <2903@holbertonschool.com>
2021-03-11 02:45:09 +0000

    Case less than 3 entries

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,15 +12,18 @@
 int main(int argc, char *argv[])
 {
 	int (*p)(int, int);
-	int num_1 = atoi(argv[1]);
-	int num_2 = atoi(argv[3]);
-	char operator = *argv[2];
+	int num_1;
+	int num_2;
+	char operator;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num_1 = atoi(argv[1]);
+	num_2 = atoi(argv[3]);
+	operator = *argv[2];
 	p = get_op_func(argv[2]);
 	if (p == NULL)
 	{
