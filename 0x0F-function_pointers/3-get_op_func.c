90f4edcb53f22f2b644697494b0af9140646a841
Adriana Echeverri <2903@holbertonschool.com>
2021-03-11 01:48:33 +0000

    Null case to option selecter

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 ? 1 : ((s[0] == (ops[2].op)[0]) ? 2 : ((s[0] == (ops[3].op)[0])
 ? 3 : ((s[0] == (ops[4].op)[0]) ? 4 : 5)))));
 
-	if (i == 5)
+	if (i == 5 || s == NULL)
 	{
 		return (NULL);
 	}
