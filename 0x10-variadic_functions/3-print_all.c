28bc6bddae530e8133c45b90daa6c2b4324d17d5
Adriana Echeverri <2903@holbertonschool.com>
2021-03-11 22:40:54 +0000

    Switch case file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ *
+ * Return: None.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list all_entries;
+	char letter, *s;
+	unsigned int index = 0;
+
+	va_start(all_entries, format);
+	while (format[index])
+	{
+		letter = format[index];
+		switch (letter)
+		{
+		case 'c':
+			printf("%c", va_arg(all_entries, int));
+			break;
+		case 's':
+			s = va_arg(all_entries, char *);
+			if (s == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", s);
+			break;
+		case 'i':
+			printf("%d", va_arg(all_entries, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(all_entries, double));
+			break;
+		}
+		if (format[index + 1] && (letter == 'c'
+|| letter == 's' || letter == 'i' || letter == 'f'))
+		{
+			printf(", ");
+		}
+		index++;
+	}
+	printf("\n");
+	va_end(all_entries);
+}
