52ec311bb02b6d88c69cdc1aab4d730266f490ce
Adriana Echeverri <2903@holbertonschool.com>
2021-03-19 17:55:59 +0000

    File to print message before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * message_before - function that prints message before main.
+ *
+ * Return: None.
+ */
+void message_before(void)
+{
+	printf("You're beat! and yet, you must
+allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -31,4 +31,6 @@ list_t *add_node_end(list_t **, const char *);
 
 void free_list(list_t *);
 
+void message_before (void) __attribute__ ((constructor));
+
 #endif /*LISTS_H*/
