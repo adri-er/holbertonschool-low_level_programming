23e70ed7e1ca7353477dc59ee23d31da8c45d4e1
Adriana Echeverri <2903@holbertonschool.com>
2021-03-19 15:26:31 +0000

    Add cases parameter is NULL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,15 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	char *str_duplicate = strdup(str);
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->str = str_duplicate;
 	for (i = 0; str_duplicate[i]; i++)
 	{
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,6 +15,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	int index;
 	char *str_copy;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
