6e14871646aff55a04d28f98de3f8005be96e5a7
Adriana Echeverri <2903@holbertonschool.com>
2021-03-23 14:12:41 +0000

    Add case where there is no list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_structure;
 	listint_t *position = *head;
 
-	if (*head == NULL || head == NULL || idx < 0)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
