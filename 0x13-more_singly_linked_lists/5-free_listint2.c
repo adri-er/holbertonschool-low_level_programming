0f078b778c6dae632efa9a1f80cfdae27ce8f7b4
Adriana Echeverri <2903@holbertonschool.com>
2021-03-23 15:12:21 +0000

    Add case head is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		(*head) = temp;
+		while (*head)
+		{
+			temp = (*head)->next;
+			free(*head);
+			(*head) = temp;
+		}
+		head = NULL;
 	}
-	head = NULL;
 }
