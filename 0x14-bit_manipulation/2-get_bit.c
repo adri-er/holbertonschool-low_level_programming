8db71e81c8e5bd382a2fa082af91c4794e6d10bb
Adriana Echeverri <2903@holbertonschool.com>
2021-03-25 20:39:29 +0000

    File to detect index value in a binary number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to get its bit.
+ * @index: index, starting from 0 of the bit you want to get.
+ *
+ * Return: value of bit at index or -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int length = 0;
+
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	while (n >> length)
+	{
+		length++;
+	}
+	if (index > (length - 1))
+	{
+		return (-1);
+	}
+	if (((n >> index) ^ 1) == ((n >> index) + 1))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+
+}
