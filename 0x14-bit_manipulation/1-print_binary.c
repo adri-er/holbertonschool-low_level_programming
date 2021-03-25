c01b34db19a562a2ee91ac1716f714a39c471b72
Adriana Echeverri <2903@holbertonschool.com>
2021-03-25 22:40:18 +0000

    Change condition to validate if odd or even

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,27 +8,29 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int copy = n;
 	long int length = 0;
+	long int index = 0;
 
 	if (n == 0 || n == 1)
 	{
 		_putchar(n + '0');
 		return;
 	}
-	while (n >> length)
+	while (copy >> index)
 	{
-		length++;
+		index++;
 	}
-	length--;
-	while (length != 0)
+	length = index - 1;
+	while (length >= 0)
 	{
-		if (((n >> length) ^ 1) == ((n >> length) + 1))
+		if (((copy >> length) & 1))
 		{
-			_putchar('0');
+			_putchar('1');
 		}
 		else
 		{
-			_putchar('1');
+			_putchar('0');
 		}
 		length--;
 	}
