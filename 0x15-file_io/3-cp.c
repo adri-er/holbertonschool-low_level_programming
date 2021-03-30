5df75a015df3e59d971ef38bdd76b35df884d872
Adriana Echeverri <adriana.er98@hotmail.com>
2021-03-30 21:01:47 +0000

    Consider case of more than 1024 bytes to print

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,42 +5,46 @@
  * @argc: amount of arguments introduced, has to be 2.
  * @argv: pointer to arguments introduced.
  *
- * Return: always 0.
+ * Return: SUCCESS or exit.
  */
 int main(int argc, char *argv[])
 {
-	int file_descriptor_to, file_descriptor_from, status, status_2;
+	int file_descriptor_to, file_descriptor_from, status = 1024, status_2;
 	char buffer[1024];
 
 	check_arg(argc, argv[2]);
 	file_descriptor_from = open(argv[1], O_RDONLY);
 	if (file_descriptor_from == -1)
-	{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_descriptor_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_descriptor_to == -1)
-	{		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	status = read(file_descriptor_from, buffer, 1024);
-	if (status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}	status_2 = write(file_descriptor_to, buffer, status);
-	if (status != status_2 || status_2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	status = close(file_descriptor_from);
-	status_2 = close(file_descriptor_to);
-	if (status_2 == -1)
+	while (status == 1024)
+	{
+		status = read(file_descriptor_from, buffer, 1024);
+		if (status == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		status_2 = write(file_descriptor_to, buffer, status);
+		if (status_2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(file_descriptor_to) == -1)
 	{		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor_to);
 		exit(100);
 	}
-	if (status == -1)
+	if (close(file_descriptor_from) == -1)
 	{		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor_from);
 		exit(100);
 	}
