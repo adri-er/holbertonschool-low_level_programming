25e561baa6a394e9a27c46e3d764ac6d6fdf65a7
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-28 15:05:41 -0500

    Add edge cases when searching for key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     unsigned long int position;
     hash_node_t *hash_node_list;
 
+    if (ht == NULL || key == NULL)
+    {
+        return (NULL);
+    }
     position = key_index((const unsigned char *)key, ht->size);
     hash_node_list = ht->array[position];
 	while (hash_node_list != NULL)
