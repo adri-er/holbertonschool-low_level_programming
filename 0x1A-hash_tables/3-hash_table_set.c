d20f816796ced3713442118774dd23837b5c845a
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-28 14:47:35 -0500

    Refactorize and add condition strdup fails

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,39 +14,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_hash_node, *hash_node_list;
 
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
-	{
 		return (0);
-	}
+
 	position = key_index((const unsigned char *)key, ht->size);
 	new_hash_node = malloc(sizeof(hash_node_t));
-	if (new_hash_node == NULL)
+	new_hash_node->key = (char *)strdup(key);
+	new_hash_node->value = (char *)strdup(value);
+	if (new_hash_node == NULL || new_hash_node->key == NULL ||
+		new_hash_node->value == NULL)
 	{
+		free(new_hash_node);
+		free(new_hash_node->key);
+		free(new_hash_node->value);
 		return (0);
 	}
-	new_hash_node->key = (char *)key;
-	new_hash_node->value = (char *)strdup(value);
 	if (ht->array[position] == NULL)
 	{
 		ht->array[position] = new_hash_node;
 		new_hash_node->next = NULL;
 		return (1);
 	}
-	else
+	hash_node_list = ht->array[position];
+	while (hash_node_list != NULL)
 	{
-		hash_node_list = ht->array[position];
-		while (hash_node_list != NULL)
+		if (strcmp(hash_node_list->key, key) == 0)
 		{
-			if (strcmp(hash_node_list->key, key) == 0)
-			{
-				free(new_hash_node->value);
-				free(new_hash_node);
-				ht->array[position]->value = (char *)value;
-				return (1);
-			}
-			hash_node_list = hash_node_list->next;
+			free(new_hash_node->value);
+			free(new_hash_node);
+			ht->array[position]->value = (char *)value;
+			return (1);
 		}
-		new_hash_node->next = ht->array[position];
-		ht->array[position] = new_hash_node;
-		return (1);
+		hash_node_list = hash_node_list->next;
 	}
+	new_hash_node->next = ht->array[position];
+	ht->array[position] = new_hash_node;
+	return (1);
 }
