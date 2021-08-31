33927f5f89b94a1b2751b23214d9e26518520afe
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-31 12:17:54 -0500

    [STYLE] (Add new line at end of file)

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 int linear_search(int *, size_t, int);
 int binary_search(int *, size_t, int);
 void print_array(int *, size_t);
+int binary_search_position(int *, size_t, int, int);
 
 #endif /* search_alg */
